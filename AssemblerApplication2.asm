18db14f6bd1067ce969337255e63a97f88cabe49
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2025-03-27 14:51:45 -0300

    Update AssemblerApplication2.asm

--- AssemblerApplication2.asm
+++ AssemblerApplication2.asm
@@ -18,11 +18,12 @@ start:
 clr r16 ; r16 <- 0
 out DDRD, r16 ; PORTD é IN
 ldi r16, 0xFF ; r16 <- 255 ou 1111 1111
+out PORTD, r16 ; liga pull-up em PORTD
 out DDRB, r16 ; PORTB é out
 
 loop:
 in r16, PIND ; ler PORTD (A) e salvar em r16
-st A, r16 ; A  <- r16
+sts A, r16 ; A  <- r16
 mov r17, r16 ; r17 <- r16 (A)
 add r17, r16 ; r17 <- r17 + r16 ou (2*x)
 
@@ -32,4 +33,4 @@ add r17, r0 ; r17 + r0 ou 2*A + A^2
 st B, r17 ; B <- r17
 out PORTB, r17 ; saída (PORTB) <- r17 
  
-rjmp loop
+jmp loop
